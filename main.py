172a510c3da245697e2372fbb5196c9a83967175
heymaker279 <heymaker@yandex.ru>
2022-04-07 07:19:32 +0500

    add_datetime_now

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from application.salary import calculate_salary
 from application.db.people import get_employees
+from _datetime import datetime
+print(datetime.now())
 calculate_salary()
 get_employees()
